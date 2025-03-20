4ff78b65ed67a44c9f1cb139b085ac730b5b03ef
dave11 <davecarandang9@gmail.com>
2025-03-20 15:54:32 +0800

    Update Route again

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,6 @@
 use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('category.index');
-});
-
+Route::redirect('/', '/category');
 
 Route::resource('category', CategoryController::class);
